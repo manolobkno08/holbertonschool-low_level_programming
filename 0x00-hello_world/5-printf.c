d6459789cf026fb02a78d4310c0f1265d7f217fb
Manuel Gomez <manologm10@hotmail.com>
2021-06-17 21:35:23 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Aprendiendo C
+ * Return: Terminado 0 (FIN)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
