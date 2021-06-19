4d0424dcd8e5b9175fac209c3145c4fdd2fb41d7
Manuel Gomez <manologm10@hotmail.com>
2021-06-19 01:33:15 +0000

    aA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Task 3
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int aA;
+
+	for (aA = 'a'; aA <= 'z'; aA++)
+	putchar(aA);
+	putchar('\n');
+
+	for (aA = 'A'; aA <= 'Z'; aA++)
+	putchar(aA);
+
+	return (0);
+
+}
