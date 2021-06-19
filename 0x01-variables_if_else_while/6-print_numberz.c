95cb09a3dfb1519df3372395d4199bfb51bd09af
Manuel Gomez <manologm10@hotmail.com>
2021-06-19 04:13:51 +0000

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Task 6
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int p;
+
+	for (p = '0'; p <= '9'; p++)
+
+	putchar(p);
+
+	putchar('\n');
+
+	return (0);
+}
