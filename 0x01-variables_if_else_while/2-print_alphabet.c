08daac1ec6b75e71f69e4659da341b3f36b6c8ef
Manuel Gomez <manologm10@hotmail.com>
2021-06-19 00:51:09 +0000

    alp

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alp = 97;
+
+	while (alp <= 122)
+	{
+		putchar(alp);
+		alp++;
+	}
+	putchar("\n");
+
+	retur(0);
+}
