fae14bc75b53bc1c2f4b1514dd25ba3544eda0b3
Manuel Gomez <manologm10@hotmail.com>
2021-06-19 04:22:35 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Task 7
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter >= 97; letter--)
+	putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
