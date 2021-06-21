496a9c82e8b2e4b00e50ce77871c4f78287940b1
Manuel Gomez <manologm10@hotmail.com>
2021-06-21 23:41:24 +0000

    project loops 3

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - Task 9
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int H = 'H';
+	int O = 'O';
+	int L = 'L';
+	int B = 'B';
+	int E = 'E';
+	int R = 'R';
+	int T = 'T';
+	int O2 = 'O';
+	int N = 'N';
+
+
+	putchar('H');
+	putchar('O');
+	putchar('L');
+	putchar('B');
+	putchar('E');
+	putchar('R');
+	putchar('T');
+	putchar('O');
+	putchar('N');
+
+	putchar('\n');
+
+	return (0);
+}
