be4f9fe36c57d01e5354d6986b4c8112a4aa62f6
Manuel Gomez <manologm10@hotmail.com>
2021-06-24 15:56:11 +0000

    task0_tes1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* positive_or_negative - tests function
+* @i: iterador
+* Return: 0
+*/
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
