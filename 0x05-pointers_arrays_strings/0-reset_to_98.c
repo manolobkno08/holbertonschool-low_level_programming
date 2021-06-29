7492fb075315c1991d12a0bb6496f162c69a9413
Manuel Gomez <manologm10@hotmail.com>
2021-06-29 19:29:49 +0000

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - funcion que asigna valor 98 a variable de puntero
+ *@n: variable puntero
+ *Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+
+#endif
