ac5792388961e90dbde378590f4a0b408d17cc77
Manuel Gomez <manologm10@hotmail.com>
2021-07-01 03:01:47 +0000

    task6.4

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,16 +11,13 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-
-		if (str[i] <= '\0')
+		if ((i % 2) == 0)
 		{
-			break;
+			_putchar(str[i]);
+
 		}
-
-		_putchar(str[i]);
-
 	}
 	_putchar(10);
 }
