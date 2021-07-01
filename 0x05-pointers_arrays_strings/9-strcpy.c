4fc730a5905d984124535acb0acdb6153c857362
Manuel Gomez <manologm10@hotmail.com>
2021-07-01 05:28:50 +0000

    task 99

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,8 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	if (dest[i] == '\0')
-	{
-		i++;
-	}
+	dest[i] = src[i];
+	dest[i] = '\0';
 
 	return (dest);
 }
