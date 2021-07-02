1ae472c0762c45e459107dc79b6c67ecb4fe2fbf
Manuel Gomez <manologm10@hotmail.com>
2021-07-02 04:07:33 +0000

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - comparar strings
+ *@s1: variable puntero char
+ *@s2: variable puntero char
+ *Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	for ( ; *s1 == *s2; s1++, s2++)
+
+		if (*s1 == 0 && *s2 == 0)
+		{
+			return (0);
+		}
+
+	return (*s1 - *s2);
+}
