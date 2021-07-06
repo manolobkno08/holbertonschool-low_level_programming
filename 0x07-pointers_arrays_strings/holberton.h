e42fa22ca8420acf77758af81fd777c4ecce96cb
Manuel Gomez <manologm10@hotmail.com>
2021-07-06 00:44:58 +0000

    task0

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int _putchar(char c);
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
