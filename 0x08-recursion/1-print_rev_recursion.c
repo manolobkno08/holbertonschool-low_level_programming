fe1e2244c58229c6493b2a7ce2e7688cdc49ead6
Manuel Gomez <manologm10@hotmail.com>
2021-07-08 00:55:54 +0000

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+*_print_rev_recursion - funcion que muestra un string en reversa
+*@s: parametro puntero char que recibe funcion
+*Return: Always 0
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+	else
+	_putchar(10);
+}
