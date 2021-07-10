00a6b89a9793a779130f9485c133127757028d8a
Manuel Gomez <manologm10@hotmail.com>
2021-07-10 18:48:19 +0000

    project_argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - funcion principal
+ *@argc: argumento entero
+ *@argv: array
+ *Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
