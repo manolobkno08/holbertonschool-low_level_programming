548da7295d2d4b9ab49c9b8303a62fbb436c459e
Manuel Gomez <manologm10@hotmail.com>
2021-07-20 04:02:57 +0000

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+/**
+ * print_dog - funcion que inicializa datos
+ *@d: asigna alias a la funcion dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %.6f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
