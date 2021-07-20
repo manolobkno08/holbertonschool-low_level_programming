1f8b1ae46be8256abebce5d9bac985b265a12b4c
Manuel Gomez <manologm10@hotmail.com>
2021-07-20 02:20:23 +0000

    task0_project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+ * struct dog - funcion que contiene struct
+ *@name: char name
+ *@age: float edad
+ *@owner: char propietario
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+};
+
+#endif
