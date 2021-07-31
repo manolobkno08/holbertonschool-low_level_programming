831792dd433d70fd8b669062c378a39713eedfd3
Manuel Gomez <manologm10@hotmail.com>
2021-07-31 21:21:56 +0000

    add_task3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,24 +1,22 @@
 #include "lists.h"
-
 /**
- * add_node - add a new node on head
- * @head: double pointer
- * @str: const string
- * Return: address of head
- */
+* add_node_end - add a new node on the end
+* @head: double pointer
+* @str: const string
+* Return: address of new element
+*/
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	/* declare pointer type list_t */
 	list_t *new_node;
+	list_t *current = *head;
 	int c = 0;
 	/* add dynamic memory to our pointer var */
 	new_node = malloc(sizeof(list_t));
-
 	/* return NUll if dont assign memory */
 	if (new_node == NULL)
 		return (NULL);
-
 	/* save the lenght of str */
 	while (str[c])
 	{
@@ -28,10 +26,19 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->str = strdup(str);
 	/* assign lenght to len->(struct) */
 	new_node->len = c;
-	/* point to the next_node */
-	new_node->next = (*head);
-	/* assign the new node as head */
-	(*head) = new_node;
-	/* return the current add node address as head */
-	return (*head);
+	/* point to NULL */
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
+
+	return (new_node);
 }
