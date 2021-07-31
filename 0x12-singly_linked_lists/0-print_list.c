8a1a7b023ddb8f85b5397a7bff3f175b3d9b03a2
Manuel Gomez <manologm10@hotmail.com>
2021-07-31 00:38:50 +0000

    proj_link

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+* print_list - leer todas las posiciones de la lista
+* @h: const puntero
+* Return: numero de nodos
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
