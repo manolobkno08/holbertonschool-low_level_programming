b6e17b4acda0ed1a13302db07b2c63154398d3c0
Manuel Gomez <manologm10@hotmail.com>
2021-08-03 05:15:32 +0000

    update_task0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,8 +6,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * struct listint_s - struct
+ * @n: var int into the struct
+ * @next: pointer next node
  * Return: Always 0.
  */
 
@@ -22,5 +23,6 @@ int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif
