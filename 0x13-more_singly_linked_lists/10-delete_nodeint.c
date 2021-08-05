96de2b7f6ca3575eb113f543f07b2e79e94ed185
manolobkno08 <manologm10@hotmail.com>
2021-08-04 20:33:17 -0500

    upd_fix_task10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,6 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = current->next;
 	}
 	next = current->next;
+	current->next = next->next;
 	free(next);
 	return (1);
 }
