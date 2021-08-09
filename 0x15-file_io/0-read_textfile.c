65c30d3ad1e05226d8146e704c09ad4f9bc01d2b
manolobkno08 <manologm10@hotmail.com>
2021-08-09 18:36:15 -0500

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,6 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* declare pointer to allocate the lenght */
 	char *buffer;
 	ssize_t rd;
 	ssize_t op;
@@ -21,19 +20,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* open file for read only assigned to var open */
 	op = open(filename, O_RDONLY);
-	/* function read assigned to var */
+
 	rd = read(op, buffer, letters);
-	/* write function */
+
 	write(STDOUT_FILENO, buffer, letters);
 
-	/* if exist any error of open return 0 */
 	if (op == -1)
 	{
 		return (0);
 	}
-	/* release buffer */
+
 	free(buffer);
 	close(op);
 	return (rd);
