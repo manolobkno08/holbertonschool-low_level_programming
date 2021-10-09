9925d289575b42bab3d34692b5ac5a158c33caf8
manolobkno08 <manologm10@hotmail.com>
2021-10-09 17:54:19 -0500

    add task 9

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - assign a key
+ * @key: new key
+ * @size: size of array
+ * Return: Always EXIT_SUCCESS.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int idx;
+
+	/*checking if key and size is null*/
+	if (key == 0 || size == 0)
+	{
+		return (0);
+	}
+
+	/*Validate position of idx*/
+	idx = hash_djb2(key) % size;
+
+	return (idx);
+}
