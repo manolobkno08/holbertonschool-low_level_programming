cbfbcfc1b599bb2dd29cff19439f5018bdbfe7c6
manolobkno08 <manologm10@hotmail.com>
2022-01-17 17:47:46 -0500

    add task 2

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * print_iterations - function that print values
+ * @array: the array of integers
+ * @start: start idx
+ * @end: final idx
+ *
+ * Return: Always 0.
+ */
+
+int print_iterations(int *array, int start, int end)
+{
+	int i;
+
+	for (i = start; i < end; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("%d\n", array[end]);
+
+	return (0);
+}
+
+/**
+ * jump_search - function that search index
+ * value in an array of integers using the Linear search algorithm
+ * @array: the array of integers
+ * @size: the size of the array
+ * @value: the value to search
+ *
+ * Return: Always 0.
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+
+	size_t middle;
+	size_t start = 0;
+	size_t end = size - 1;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (start <= end)
+	{
+		printf("Searching in array: ");
+		print_iterations(array, start, end);
+		middle = (start + end) / 2;
+
+		if (array[middle] < value)
+		{
+			start = middle + 1;
+		}
+		else if (array[middle] > value)
+		{
+			end = middle - 1;
+		}
+		else
+			return (middle);
+	}
+
+	return (-1);
+}
